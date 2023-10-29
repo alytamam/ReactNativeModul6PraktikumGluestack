40fa0c7fcf77f44d473a2e8864f4529dc36eee25
alytamam <alytmaam@gmail.com>
2023-10-29 23:37:17 +0700

    update news detail

--- app/news-detail.js
+++ app/news-detail.js
@@ -1,19 +1,26 @@
-import { Heading, Text, Center } from "@gluestack-ui/themed";
+import { Heading, Text, Center,Image,Divider } from "@gluestack-ui/themed";
 import { Header } from "../components";
 import { useLocalSearchParams } from "expo-router";
+// import { Image } from "expo-image";
 
 const NewsDetail = () => {
   const params = useLocalSearchParams();
   return (
     <>
       <Header title={"News"} withBack={true} />
-      <Center flex={1} padding={"$4"}>
+      <Center flex={1} padding={"$4"}>      
         <Heading>News Detail</Heading>
-        <Text textAlign="center">{params.title}</Text>
+        <Image width="$full"  height={187} marginTop={250} borderRadius="$none" 
+         source={{uri: params.image,
+        }}
+       />
+        <Text paddingRight={200} fontWeight="$bold" paddingVertical={9}>{params.date}</Text>
+        <Text textAlign="justify" paddingRight={30} fontWeight="$extrabold" fontSize={20}>{params.title}</Text>
+        <Divider marginVertical={10}/>
+        <Text textAlign="justify">{params.content}</Text>
       </Center>
     </>
   );
 };
 
-export default NewsDetail;
-
+export default NewsDetail;
